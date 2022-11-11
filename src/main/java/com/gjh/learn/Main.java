5ccf73e8551b55e56488f534cefead854b2f9642
kevin <guangjinhai@163.com>
2022-11-11 19:35:43 +0800

    spring boot web base init

---
+++ src/main/java/com/gjh/learn/Main.java
@@ -0,0 +1,16 @@
+package com.gjh.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * created on 2022/11/11
+ *
+ * @author kevinlights
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
